9e1dc835d1828cee4fa91e32575b0490cbf1c3ce
pavel <pavel@DESKTOP-CH79O77>
2022-09-21 16:56:58 -0600

    Agregar archivos de proyecto.

---
+++ Meno constructor/Meno constructor/Menoclass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meno_constructor
+{
+    // nombre de la clase
+    internal class Menoclass
+    {
+        //campo propiedades, como es
+        private int n1;
+        private int n2;
+
+        //constructor
+        public Menoclass (int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+        //metodo, accion, lo que hace
+        public int menos()
+        {
+            return (n1 - n2);
+
+        }
+    }
+}
